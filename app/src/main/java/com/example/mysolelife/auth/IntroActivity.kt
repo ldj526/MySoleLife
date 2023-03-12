14bfd987be12957528492aaea77989cf39fdd963
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-12 20:13:55 +0900

    feat: IntroActivity 추가

---
+++ app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mysolelife.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mysolelife.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
