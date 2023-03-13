c439847123cfbeeac838656dbb8d237165896614
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 16:40:02 +0900

    feat: JoinActivity DataBinding 추가

--- app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
+++ app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mysolelife.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.mysolelife.R
+import com.example.mysolelife.databinding.ActivityJoinBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -11,23 +13,26 @@ import com.google.firebase.ktx.Firebase
 class JoinActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityJoinBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
 
         // Initialize Firebase Auth
         auth = Firebase.auth
 
-        auth.createUserWithEmailAndPassword("abc@abc.com", "abcdabcd")
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
-                }
-            }
+        // DataBinding 추가
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+//        auth.createUserWithEmailAndPassword("abc@abc.com", "abcdabcd")
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+//                } else {
+//                    // If sign in fails, display a message to the user.
+//                    Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+//                }
+//            }
     }
 }
