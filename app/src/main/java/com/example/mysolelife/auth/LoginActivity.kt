d565ae2f71a95bdfac34355d7c92b1bc41df9e00
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 16:52:50 +0900

    feat: LoginActivity DataBinding 설정

--- app/src/main/java/com/example/mysolelife/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mysolelife/auth/LoginActivity.kt
@@ -2,11 +2,18 @@ package com.example.mysolelife.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.mysolelife.R
+import com.example.mysolelife.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        // DataBinding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
     }
 }
