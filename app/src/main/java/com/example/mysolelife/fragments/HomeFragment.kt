98bea990002f18df0d1e978ed56c7f41f8584c04
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 17:54:52 +0900

    feat: DataBinding 추가 후 바텀 네비게이션 바 생성

---
+++ app/src/main/java/com/example/mysolelife/fragments/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.mysolelife.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.mysolelife.R
+import com.example.mysolelife.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // DataBinding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        return binding.root
+    }
+}
