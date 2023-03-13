180f53322005e69aa3df76b3383f05e0e7f15c1b
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 16:13:12 +0900

    feat: DataBinding 추가

--- app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
@@ -1,12 +1,29 @@
 package com.example.mysolelife.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.mysolelife.R
+import com.example.mysolelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        // DataBinding 추가
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
