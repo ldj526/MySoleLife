733a279965d7beb696f52fe23631ee0f37ac357a
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 17:06:31 +0900

    feat: 비회원 로그인 기능 추가

--- app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/mysolelife/auth/IntroActivity.kt
@@ -2,16 +2,26 @@ package com.example.mysolelife.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.example.mysolelife.MainActivity
 import com.example.mysolelife.R
 import com.example.mysolelife.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         // DataBinding 추가
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
@@ -24,6 +34,26 @@ class IntroActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+
+                        // Activity 이동 후 뒤로가기 눌렀을 때 이전 화면 없애주는 것
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 }
