01c89e07f33fe2127783ac8b430f0ee804573cee
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 16:31:24 +0900

    feat: 회원가입에 대한 기초 틀 만들기

--- app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
+++ app/src/main/java/com/example/mysolelife/auth/JoinActivity.kt
@@ -2,11 +2,32 @@ package com.example.mysolelife.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mysolelife.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        auth.createUserWithEmailAndPassword("abc@abc.com", "abcdabcd")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
