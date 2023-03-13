928a194f6bbab072dc2f027933a95ea509669fab
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 17:16:06 +0900

    feat: 로그인과 로그아웃 상태에 따른 화면 이동
    로그인이 되어 있으면 스플래시 화면이 나온 뒤 메인 액티비티로 이동하고
    로그아웃이 되어 있는 상태면 스플래시 화면이 나온 뒤 인트로 액티비티로 이동한다.

--- app/src/main/java/com/example/mysolelife/SplashActivity.kt
+++ app/src/main/java/com/example/mysolelife/SplashActivity.kt
@@ -1,19 +1,36 @@
 package com.example.mysolelife
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mysolelife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        if (auth.currentUser?.uid == null) {
+            // 로그인 되어 있을 시 MainActivity 로 이동
+            Log.d("SplashActivity", "null")
+        } else {
+            // 로그아웃이 되어 있을 시 IntroActivity 로 이동
+            Log.d("SplashActivity", "not null")
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+        }
     }
 }
