db90b73e025509af2b49e2447b9d2a2797899a7c
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-13 17:03:55 +0900

    feat: 로그아웃 기능 추가

--- app/src/main/java/com/example/mysolelife/MainActivity.kt
+++ app/src/main/java/com/example/mysolelife/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.mysolelife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.mysolelife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+            // 로그아웃
+            auth.signOut()
+            val intent = Intent(this, IntroActivity::class.java)
+
+            // Activity 이동 후 뒤로가기 눌렀을 때 이전 화면 없애주는 것
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
     }
 }
