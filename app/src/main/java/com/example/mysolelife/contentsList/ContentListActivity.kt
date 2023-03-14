3cf8a0ee4c3a9e806aecab9dd2e8331b2f0ad131
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-14 23:14:13 +0900

    feat: Firebase RealtimeDatabase 추가

--- app/src/main/java/com/example/mysolelife/contentsList/ContentListActivity.kt
+++ app/src/main/java/com/example/mysolelife/contentsList/ContentListActivity.kt
@@ -8,11 +8,41 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mysolelife.R
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ContentListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_list)
+
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("contents")
+
+        myRef.push().setValue(
+            ContentModel(
+                "title1",
+                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png",
+                "https://philosopher-chan.tistory.com/1235"
+            )
+        )
+
+        myRef.push().setValue(
+            ContentModel(
+                "title2",
+                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png",
+                "https://philosopher-chan.tistory.com/1236"
+            )
+        )
+
+        myRef.push().setValue(
+            ContentModel(
+                "title3",
+                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png",
+                "https://philosopher-chan.tistory.com/1237"
+            )
+        )
 
         val rv: RecyclerView = findViewById(R.id.rv)
 
