0bb6212005157f8e0fa7cef069830ae72a155b5e
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-15 22:30:07 +0900

    feat: BookmarkModel을 만들고 database의 bookmark에서 uid값 받아오기

--- app/src/main/java/com/example/mysolelife/contentsList/ContentListActivity.kt
+++ app/src/main/java/com/example/mysolelife/contentsList/ContentListActivity.kt
@@ -1,15 +1,14 @@
 package com.example.mysolelife.contentsList
 
 import android.content.ContentValues.TAG
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mysolelife.R
+import com.example.mysolelife.utils.FBAuth
+import com.example.mysolelife.utils.FBRef
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -67,77 +66,26 @@ class ContentListActivity : AppCompatActivity() {
 
         rv.adapter = rvAdapter
         rv.layoutManager = GridLayoutManager(this, 2)
+
+        getBookmarkData()
+    }
+
+    private fun getBookmarkData() {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                for (dataModel in dataSnapshot.children) {
+                    Log.d("getBookmarkData", dataModel.key.toString())
+                    Log.d("getBookmarkData", dataModel.toString())
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        // Bookmark 에서 uid 값 가져오기
+        FBRef.bookmarkRef.child(FBAuth.getUid()).addValueEventListener(postListener)
     }
 }
-
-
-//        myRef.push().setValue(
-//            ContentModel(
-//                "title1",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1235"
-//            )
-//        )
-//
-//        myRef.push().setValue(
-//            ContentModel(
-//                "title2",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1236"
-//            )
-//        )
-//
-//        myRef.push().setValue(
-//            ContentModel(
-//                "title3",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1237"
-//            )
-//        )
-
-//        val myRef2 = database.getReference("contents2")
-//        myRef2.push().setValue(
-//            ContentModel(
-//                "title4",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1235"
-//            )
-//        )
-//
-//        myRef2.push().setValue(
-//            ContentModel(
-//                "title5",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1236"
-//            )
-//        )
-//
-//        myRef2.push().setValue(
-//            ContentModel(
-//                "title6",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1237"
-//            )
-//        )
-
-//        items.add(
-//            ContentModel(
-//                "title1",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1235"
-//            )
-//        )
-//        items.add(
-//            ContentModel(
-//                "title2",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1236"
-//            )
-//        )
-//        items.add(
-//            ContentModel(
-//                "title3",
-//                "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png",
-//                "https://philosopher-chan.tistory.com/1237"
-//            )
-//        )
