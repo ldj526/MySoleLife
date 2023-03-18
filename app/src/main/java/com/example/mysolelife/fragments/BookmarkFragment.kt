b21370441898556921f5a4710a09bebee4846436
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-18 22:15:55 +0900

    feat: 북마크한 정보들을 BookmarkFragment로 가져오는 기능 추가

--- app/src/main/java/com/example/mysolelife/fragments/BookmarkFragment.kt
+++ app/src/main/java/com/example/mysolelife/fragments/BookmarkFragment.kt
@@ -1,6 +1,8 @@
 package com.example.mysolelife.fragments
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +11,16 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.mysolelife.R
 import com.example.mysolelife.databinding.FragmentBookmarkBinding
+import com.example.mysolelife.utils.FBAuth
+import com.example.mysolelife.utils.FBRef
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class BookmarkFragment : Fragment() {
     private lateinit var binding: FragmentBookmarkBinding
+
+    private val TAG = BookmarkFragment::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +32,12 @@ class BookmarkFragment : Fragment() {
     ): View? {
         // DataBinding
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmark, container, false)
+
+        // 전체 카테고리에 있는 컨텐츠 데이터들을 가져오기
+        getCategoryData()
+
+        // 사용자가 북마크한 정보를 가져오기
+        getBookmarkData()
 
         binding.homeTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_bookmarkFragment_to_homeFragment2)
@@ -41,5 +56,42 @@ class BookmarkFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun getCategoryData() {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                for (dataModel in dataSnapshot.children) {
+                    Log.d(TAG, dataModel.toString())
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(ContentValues.TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.category1.addValueEventListener(postListener)
+        FBRef.category2.addValueEventListener(postListener)
+    }
+
+    private fun getBookmarkData() {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                // Get Post object and use the values to update the UI
+                for (dataModel in dataSnapshot.children) {
+                    Log.e(TAG, dataModel.toString())
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(ContentValues.TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        // Bookmark 에서 uid 값 가져오기
+        FBRef.bookmarkRef.child(FBAuth.getUid()).addValueEventListener(postListener)
     }
 }
--- app/src/main/java/com/example/mysolelife/utils/FBRef.kt
+++ app/src/main/java/com/example/mysolelife/utils/FBRef.kt
@@ -6,6 +6,11 @@ import com.google.firebase.ktx.Firebase
 class FBRef {
     companion object {
         private val database = Firebase.database
+
+        val category1 = database.getReference("contents")
+        val category2 = database.getReference("contents2")
+
         val bookmarkRef = database.getReference("bookmark_list")
+
     }
 }
