b11a2cd93ec3e5f3243983887a5c431d565e03e6
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-19 22:55:35 +0900

    feat: 이미지를 Firebase storage에 올리는 기능 추가

--- app/src/main/java/com/example/mysolelife/board/BoardWriteActivity.kt
+++ app/src/main/java/com/example/mysolelife/board/BoardWriteActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mysolelife.board
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
@@ -11,6 +13,9 @@ import com.example.mysolelife.R
 import com.example.mysolelife.databinding.ActivityBoardWriteBinding
 import com.example.mysolelife.utils.FBAuth
 import com.example.mysolelife.utils.FBRef
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
 
 class BoardWriteActivity : AppCompatActivity() {
 
@@ -31,18 +36,52 @@ class BoardWriteActivity : AppCompatActivity() {
             Log.d(TAG, title)
             Log.d(TAG, content)
 
+            /**
+            이미지 이름에 대한 정보를 모르기 때문에
+            이미지 이름을 key 값으로 해주기 위해 key 값을 미리 생성한다.
+             */
+            val key = FBRef.boardRef.push().key.toString()
+
             // 게시글 쓰기
             FBRef.boardRef
-                .push()
+                .child(key)
                 .setValue(BoardModel(title, content, uid, time))
 
             Toast.makeText(this, "게시글 입력 완료", Toast.LENGTH_LONG).show()
+
+            imageUpload(key)
             finish()
         }
 
         binding.imageArea.setOnClickListener {
             val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             startActivityForResult(gallery, 100)
+        }
+    }
+
+    private fun imageUpload(key: String) {
+        // Get the data from an ImageView as bytes
+
+        val storage = Firebase.storage
+        // Create a storage reference from our app
+        val storageRef = storage.reference
+        // Create a reference to "mountains.jpg"
+        val mountainsRef = storageRef.child("$key.png")
+
+        val imageView = binding.imageArea
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
         }
     }
 
