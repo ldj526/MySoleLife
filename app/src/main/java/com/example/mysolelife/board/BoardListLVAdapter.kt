7ab6b848a4658f9a77ef95b71c592ad45b4181fa
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-24 20:13:06 +0900

    feat: 자신이 쓴 게시글은 다른 색으로 표현해주는 기능 추가

--- app/src/main/java/com/example/mysolelife/board/BoardListLVAdapter.kt
+++ app/src/main/java/com/example/mysolelife/board/BoardListLVAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.mysolelife.board
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.mysolelife.R
+import com.example.mysolelife.utils.FBAuth
 
 class BoardListLVAdapter(val boardList: MutableList<BoardModel>) : BaseAdapter() {
     override fun getCount(): Int {
@@ -22,14 +25,20 @@ class BoardListLVAdapter(val boardList: MutableList<BoardModel>) : BaseAdapter()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var view = convertView
-        if (view == null) {
-            view = LayoutInflater.from(parent?.context)
-                .inflate(R.layout.board_list_item, parent, false)
-        }
+
+        view = LayoutInflater.from(parent?.context)
+            .inflate(R.layout.board_list_item, parent, false)
+
+        val itemLinearLayoutView = view?.findViewById<LinearLayout>(R.id.itemView)
 
         val title = view?.findViewById<TextView>(R.id.titleArea)
         val content = view?.findViewById<TextView>(R.id.contentArea)
         val time = view?.findViewById<TextView>(R.id.timeArea)
+
+        // 자신이 쓴 글은 다른 색으로 표현하기
+        if (boardList[position].uid.equals(FBAuth.getUid())) {
+            itemLinearLayoutView?.setBackgroundColor(Color.parseColor("#fefae0"))
+        }
 
         title!!.text = boardList[position].title
         content!!.text = boardList[position].content
