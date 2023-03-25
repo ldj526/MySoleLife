a8f9f9f68a1cb1a3b379ba7eec850138f5625983
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-03-25 21:18:29 +0900

    feat: 레이아웃 디자인 정리

--- app/src/main/java/com/example/mysolelife/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/mysolelife/fragments/HomeFragment.kt
@@ -1,19 +1,36 @@
 package com.example.mysolelife.fragments
 
+import android.content.ContentValues
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mysolelife.R
+import com.example.mysolelife.contentsList.BookmarkRVAdapter
+import com.example.mysolelife.contentsList.ContentModel
 import com.example.mysolelife.databinding.FragmentHomeBinding
+import com.example.mysolelife.utils.FBRef
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+
+    private val TAG = HomeFragment::class.java.simpleName
+
+    val bookmarkIdList = mutableListOf<String>()
+    val items = ArrayList<ContentModel>()
+    val itemKeyList = ArrayList<String>()
+
+    lateinit var rvAdapter: BookmarkRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +58,39 @@ class HomeFragment : Fragment() {
         binding.storeTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment2_to_storeFragment)
         }
+        getCategoryData()
+
+        rvAdapter = BookmarkRVAdapter(requireContext(), items, itemKeyList, bookmarkIdList)
+
+        val rv: RecyclerView = binding.mainRV
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = GridLayoutManager(requireContext(), 2)
 
         return binding.root
     }
+
+    private fun getCategoryData() {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                for (dataModel in dataSnapshot.children) {
+                    Log.d(TAG, dataModel.toString())
+                    val item = dataModel.getValue(ContentModel::class.java)
+                    items.add(item!!)
+                    itemKeyList.add(dataModel.key.toString())
+                }
+                // Sync
+                rvAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(ContentValues.TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.category1.addValueEventListener(postListener)
+        FBRef.category2.addValueEventListener(postListener)
+    }
+
 }
--- app/src/main/java/com/example/mysolelife/fragments/StoreFragment.kt
+++ app/src/main/java/com/example/mysolelife/fragments/StoreFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import com.example.mysolelife.R
-import com.example.mysolelife.databinding.FragmentStoreBinding
 
 class StoreFragment : Fragment() {
-    private lateinit var binding: FragmentStoreBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,25 +17,11 @@ class StoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // DataBinding
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_store, container, false)
+        val view = inflater.inflate(R.layout.fragment_store, container, false)
 
-        binding.homeTap.setOnClickListener {
-            it.findNavController().navigate(R.id.action_storeFragment_to_homeFragment2)
-        }
+        val webView: WebView = view.findViewById(R.id.storeWebView)
+        webView.loadUrl("https://m.naver.com/")
 
-        binding.tipTap.setOnClickListener {
-            it.findNavController().navigate(R.id.action_storeFragment_to_tipFragment)
-        }
-
-        binding.talkTap.setOnClickListener {
-            it.findNavController().navigate(R.id.action_storeFragment_to_talkFragment)
-        }
-
-        binding.bookmarkTap.setOnClickListener {
-            it.findNavController().navigate(R.id.action_storeFragment_to_bookmarkFragment)
-        }
-
-        return binding.root
+        return view
     }
 }
